6cd52b36c91c083b607650ef68b89f780f3e3057
Renato Santos <Renato Santos@DESKTOP-7EEPT47>
2023-02-15 21:44:58 +0000

    Add project files.

---
+++ TestApp/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using TestApp;
+using static System.Net.Mime.MediaTypeNames;
+
+string endpoint = "https://apidemo.sagex3.com/demo/service/X3CLOUDV2_SEED/api";
+
+
+var client = new GraphQLHttpClient(endpoint, new NewtonsoftJsonSerializer());
+//client.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + Convert.ToString(Application.Current.Properties["Login"]));
+
+
+
+//Comando SQL
+//Comando GRAPH SQL to like sage x3 { xtremX3MasterData { supplier { query (filter:"{ companyName: {_regex:'Pesquisar'}}"){ edges { node { _id companyName } } } } } }
+
+
+
+var request = new GraphQLRequest
+{
+    Query = @"
+  {xtremX3MasterData {
+    customer {
+      query {
+        edges {
+          node {
+            _id
+            companyName
+          }
+        }
+      }
+    }
+  }
+}"
+};
+
+var response = await client.SendQueryAsync<Data>(request);
+
+
+Console.WriteLine("Hello, World!");
---
+++ TestApp/SupplierResponse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Customer
+    {
+        public Query query { get; set; }
+    }
+
+    public class Data
+    {
+        public XtremX3MasterData xtremX3MasterData { get; set; }
+    }
+
+    public class Edge
+    {
+        public Node node { get; set; }
+    }
+
+    public class Node
+    {
+        public string _id { get; set; }
+        public string companyName { get; set; }
+    }
+
+    public class Query
+    {
+        public List<Edge> edges { get; set; }
+    }
+
+    public class Root
+    {
+        public Data data { get; set; }
+    }
+
+    public class XtremX3MasterData
+    {
+        public Customer customer { get; set; }
+    }
+
+
+
+}
